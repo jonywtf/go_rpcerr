add332b3b7fb14c0b88a3e8624d7cc117a24a968
jonywtf <jonywtf@localhost>
2015-11-27 00:09:10 +0300

    to windows system

--- build.bat
+++ build.bat
@@ -5,10 +5,13 @@ set GOPATH=%~dp0
 set GOARCH=amd64
 set GOOS=windows
 
-cd %GOPATH%
-
-set TARGET_PATH=%GOPATH%bin\gen_err.exe
-del %TARGET_PATH%
 cd %GOPATH%src\
+
+set TARGET_PATH=C:\Windows\System32\gen_err.exe
+del %TARGET_PATH%
 go build -i -o %TARGET_PATH%
-cd %GOPATH%
+
+set TARGET_PATH=C:\Windows\SysWOW64\gen_err.exe
+del %TARGET_PATH%
+go build -i -o %TARGET_PATH%
+pause
--- src/main.go
+++ src/main.go
@@ -8,15 +8,19 @@ import (
 	"os"
 	"strings"
 	"path/filepath"
+	"log"
 )
 
 func main() {
 	if len(os.Args) < 2 {
     gopath_envr := os.Getenv("GOPATH")
-    gopaths:= strings.Split(gopath_envr, ";")
-    for _, gopath := range gopaths {
-      src_path, _ := filepath.Abs(gopath + "/src/")
-      GenErrorInSrc(src_path)
+    if len(gopath_envr) > 0 {
+      log.Println("GOPATH: ", gopath_envr)
+      gopaths:= strings.Split(gopath_envr, ";")
+      for _, gopath := range gopaths {
+        src_path, _ := filepath.Abs(gopath + "/src/")
+        GenErrorInSrc(src_path)
+      }
     }
 	} else {
     GenErrorInSrc(os.Args[1])
