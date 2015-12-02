63e53feb261473fac7c97fa54b51cde9dfe51814
jonywtf <jonywtf37@gmail.com>
2015-12-02 08:20:11 +0300

    build to bin, system folders

--- build.bat
+++ build.bat
@@ -7,6 +7,10 @@ set GOOS=windows
 
 cd %GOPATH%src\
 
+set TARGET_PATH=%GOPATH%bin\gen_err.exe
+del %TARGET_PATH%
+go build -i -o %TARGET_PATH%
+
 set TARGET_PATH=C:\Windows\System32\gen_err.exe
 del %TARGET_PATH%
 go build -i -o %TARGET_PATH%
@@ -14,4 +18,5 @@ go build -i -o %TARGET_PATH%
 set TARGET_PATH=C:\Windows\SysWOW64\gen_err.exe
 del %TARGET_PATH%
 go build -i -o %TARGET_PATH%
-pause
+
+cd %GOPATH%
